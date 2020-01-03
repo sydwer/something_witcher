a95b947596ff9afa6041f134750b12107fb8f420
Sydney Werbach <sydwer@gmail.com>
2020-01-02 17:08:27 -0700

    Tweaked styling to show full parchment paper background on contracts, and modified the padding and margins on info elements

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body')
 const main = document.getElementById('main')
 const enter_button = document.createElement('button')
 const contractBoard = document.createElement('div')
@@ -18,18 +19,27 @@ function storeResponse(data){
 function startGame(){
     enter_button.textContent = "ENTER"
     enter_button.className = "enter_button"
-    enter_button.addEventListener("click",loadContracts)
+    // enter_button.addEventListener("click",loadContracts)
+    enter_button.addEventListener("click",goToTown)
     banner.src= "something_witcher_this_way_comes.png"
     main.append(banner, enter_button)
     console.log("it worked")
     // loadContracts(monsters)
 }
-
-function loadContracts(){
+function goToTown(){
     main.removeChild(enter_button)
-    banner.style.height = "10rem"
+    banner.style.height = "13rem"
+    body.style.backgroundImage = "url('https://www.game-debate.com/blog/images/_id1427186261_1_7.jpg')"
+    setTimeout(function(){ loadContracts(); }, 1500)
+}
+function loadContracts(){
+    townBoard = document.createElement('div')
+    notice = document.createElement('img')
+    notice.src ="notice_board.png"
+    townBoard.className = "town_board"
     monsters.map(monster =>{
         contract = document.createElement('div')
+        header = document.createElement('h4')
         title = document.createElement('h1')
         image = document.createElement('img')
         place = document.createElement('h3')
@@ -38,15 +48,17 @@ function loadContracts(){
 
         contract.className = "contract"
 
+        header.textContent = "Monster Contract:"
         title.textContent = monster.name 
         console.log(monster.name)
         image.src =  monster.img_src
-        place.textContent = monster.place.name
+        place.textContent = `Spotted near: ${monster.place.name}`
         info.textContent = monster.description
-        reward.textContent = monster.reward_amount 
+        reward.textContent = `Reward: ${monster.reward_amount} gold` 
 
-        contract.append(title,image,place,reward)
+        contract.append(header,title,image,place,reward)
         contractBoard.appendChild(contract)
     })
-   main.appendChild(contractBoard)
+    townBoard.append(notice, contractBoard)
+    main.appendChild(townBoard)
 }
