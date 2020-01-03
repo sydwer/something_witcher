be25923457a0fa8e9890bb0c6fb015eb84a1187b
Sydney Werbach <sydwer@gmail.com>
2020-01-03 01:27:03 -0700

    added potion page and selection in, began on choosing oils function

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const body = document.querySelector('body')
 const main = document.getElementById('main')
 const enter_button = document.createElement('button')
 const contractBoard = document.createElement('div')
+const potionList = document.createElement('div')
+const oilList = document.createElement('div')
 const banner = document.createElement('img')
 const audio = new Audio('choir.mp3')
 
+
 let monsters = null
+let potions = null
+let oils = null
+
 let selected_enemy = null
+let equipped_potion = null
+let equipped_oil = null
+
 let witcher_hp = 100
 
 
@@ -21,13 +30,14 @@ fetch("http://127.0.0.1:3000/monsters")
 
 function storeResponse(data){
     monsters = data
+    // audio.play()
 }
 function startGame(){
     enter_button.textContent = "ENTER"
     enter_button.className = "enter_button"
     enter_button.addEventListener("click",goToTown)
     banner.src= "something_witcher_this_way_comes.png"
-    audio.play();
+    // audio.play();
     main.append(banner, enter_button)
 }
 function goToTown(){
@@ -84,5 +94,76 @@ function loadContracts(){
 function selectContract(event){
     selected_enemy = (monsters.find(obj => obj.name === event.target.value)) 
     console.log(selected_enemy)
+    fetch("http://127.0.0.1:3000/potions")
+    .then(response => response.json())
+    .then(storePotions)
+}
+function storePotions(data){
+    potions = data
+    makePreperations()
+}
+function makePreperations(){
+    console.log(potions)
+    body.style.backgroundImage = "url('https://media2.giphy.com/media/CMvgWSEKDUvaE/source.gif'"
+    main.removeChild(townBoard)
+    choosePotion(potions)
+}
+
+function choosePotion(potions){
+    const instructions = document.createElement('h1')
+    // const potionList = document.createElement('div')
+    instructions.textContent = "Pick Your Potion:"
+    instructions.className = "pick_your_potion"
+    potionList.className = "potion_list"
+    potions.map(potion =>{
+        const potion_card = document.createElement('div')
+        const image = document.createElement('img')
+        const name = document.createElement('h2')
+        const effect = document.createElement('h3')
+        const select_button = document.createElement('button')
+
+        potion_card.className = "potion_card"
+
+        image.src = potion.img_src
+        name.textContent = potion.name
+        effect.textContent = potion.effect
+        select_button.textContent = "Equip"
+        select_button.value = potion.name
+
+        potion_card.append(image,name,effect,select_button)
+        select_button.addEventListener("click", equipPotion)
+        potionList.append(potion_card)
+    })
+    main.append(instructions, potionList)
+}
+
+function equipPotion(event){
+    equipped_potion = (potions.find(obj => obj.name === event.target.value))
+    console.log(equipped_potion)
+    fetch("http://127.0.0.1:3000/oils")
+    .then(response => response.json())
+    .then(storeOils)
+}
+function storeOils(data){
+    oils = data
+    main.remove(potionList)
+    chooseOil(oils)
+}
+function chooseOil(oils){
+    instructions = document.createElement('h1')
+    oils.map(oil =>{
+        oilCard = document.createElement('div')
+        const image = document.createElement('img')
+        const name = document.createElement('h2')
+        const effect = document.createElement('h3')
+        const select_button = document.createElement('button')
+        image.src = ""
+    })
+   
+    instructions.className = "pick_your_oil"
+    instructions.textContent = "Choose an Oil for Your Blade:"
+    console.log(instructions.textContent)
+    main.appendChild(instructions)
     
 }
+
