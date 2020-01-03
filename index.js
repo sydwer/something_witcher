a294f699e890785a99dda349283a4bca23f85b45
Sydney Werbach <sydwer@gmail.com>
2020-01-02 23:09:27 -0700

    added audio to game, added box shadow to ontract on hover

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const main = document.getElementById('main')
 const enter_button = document.createElement('button')
 const contractBoard = document.createElement('div')
 const banner = document.createElement('img')
+const audio = new Audio('choir.mp3')
 
 let monsters = null
 let selected_enemy = null
@@ -26,16 +27,18 @@ function startGame(){
     enter_button.className = "enter_button"
     enter_button.addEventListener("click",goToTown)
     banner.src= "something_witcher_this_way_comes.png"
+    audio.play();
     main.append(banner, enter_button)
 }
 function goToTown(){
     main.removeChild(enter_button)
     banner.style.height = "10rem"
-    body.style.backgroundImage = "url('https://www.game-debate.com/blog/images/_id1427186261_1_7.jpg')"
-    setTimeout(function(){ loadContracts(); }, 1500)
+    body.style.backgroundImage = "url('https://media3.giphy.com/media/kmIwydQ8Xuhiw/giphy.gif?cid=790b76114b6b9a6f6c1ce652b9fdaf8a94c380c244ab5435&rid=giphy.gif')"
+    setTimeout(function(){ loadContracts(); }, 2100)
 }
 function loadContracts(){
     main.removeChild(banner)
+    body.style.backgroundImage = "url('https://www.game-debate.com/blog/images/_id1427186261_1_7.jpg')"
     townBoard = document.createElement('div')
     notice = document.createElement('img')
     notice.src ="notice_board.png"
